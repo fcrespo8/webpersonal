7a82fea15aa1fdeef0c43b4c52baf73af1079a8c
fcrespo8 <fcrespo8@hotmail.com>
2023-06-22 16:09:15 +0200

    about portfolio and contact views added

--- core/views.py
+++ core/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about-me/">Acerca de</a></li>
+        <li><a href="/portfolio/">Portfolio</a></li>
+        <li><a href="/contact/">Contacto</a></li>
+    </ul>
+"""
 
 def home(request):
-    return HttpResponse("<h1>Home</h1>")
+    return HttpResponse(html_base + """
+        <h2>Bienvenidos</h2>
+        <p>Esto es la portada.</p>
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Francisco y me encanta Django!</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Portfolio</h2>
+        <p>Algunos de mis trabajos</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí puedes contactar conmigo</p>
+    """)
